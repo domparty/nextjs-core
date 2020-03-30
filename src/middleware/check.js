9f61fe22faa5959194a73aff7c4fae84d91f98ca
Glenn de Haan <glenn@dehaan.cloud>
2020-03-30 19:30:24 +0200

    Added the next.js compiler check middleware

---
+++ src/middleware/check.js
@@ -0,0 +1,18 @@
+/**
+ * Check if the Next.JS compiler is ready
+ *
+ * @param core
+ */
+module.exports = next = (core) => {
+  /**
+   * Return the middleware
+   */
+  return (req, res, next) => {
+    if(req.originalUrl === "/core-ready-check-url") {
+      res.set('Content-Type', 'text/plain');
+      return res.status(200).send(`${core._nextReady}`);
+    }
+
+    next();
+  };
+};
