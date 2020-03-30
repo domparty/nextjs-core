965790fc785b905b8cc6193d4e406d6f7932629e
Glenn de Haan <glenn@dehaan.cloud>
2020-03-30 19:31:04 +0200

    Added the headers middleware

---
+++ src/middleware/headers.js
@@ -0,0 +1,18 @@
+/**
+ * Add app specific headers to the express server
+ *
+ * @param core
+ */
+module.exports = headers = (core) => {
+  /**
+   * Return the middleware
+   */
+  return (req, res, next) => {
+    if(core.config.application.dompartyHeaders) {
+      const packageInformation = require(__dirname + '/../../package.json');
+      res.set('X-DOMParty-Core', packageInformation.version);
+    }
+
+    next();
+  };
+};
